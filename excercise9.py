327e5e46a4cdd51085eb48b809b8d17b9ef03d6c
Ilham <ilhamaidarus@gmail.com>
2021-01-24 14:28:01 +0000

    Completed first task

--- excercise9.py
+++ excercise9.py
@@ -1,1 +1,11 @@
-print("hello")
+firstname = "ilham"
+lastname = "aidarus"
+
+print(firstname, lastname)
+
+namelist:[firstname,lastname]
+
+namedict = {
+    firstname: lastname
+}
+print(namedict[lastname])
