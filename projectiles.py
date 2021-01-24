41f524110ce65d8ac3cfe2231e152d593abaaea4
Sophieylchan <sophieylchan@gmail.com>
2021-01-24 17:25:57 +0000

    completed part 3

--- projectiles.py
+++ projectiles.py
@@ -15,6 +15,6 @@ print(theta)
 
 # (attempt 1) y = y0 + x math.tan(theta) - (g**2/2(v0 math.cos(theta))**2)
 # (attempt 2) y = x*tan(theta) - 1/(2*v0**2)*g*x**2/((cos(theta))**2) + y0
-y = y0 + x*tan(theta) - g*x**2 / (2*(v0*cos(theta)**2))
+y = y0 + x*tan(theta) - g*x**2 / (2*(v0*cos(theta))**2)
 
 print(y)
